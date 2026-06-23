d6b2029f56f6a9e140bc43a5f4735b5488a1c5bb
riturajtiwari608-ai <riturajtiwari608@gmail.com>
2026-06-23 17:29:38 +0530

    3.4

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
